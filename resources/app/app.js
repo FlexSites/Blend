9b990edbab0f7f40864c2fc6e6778414b055899b
Tim Collinson <trcollinson@gmail.com>
2016-04-02 17:03:11 -0600

    Begining refactor to move view out of controller.
    Former-commit-id: 5f63aae7c7116fec9b967cfa11527c8e724fe71c

--- resources/app/app.js
+++ resources/app/app.js
@@ -83,7 +83,55 @@ $('#new').on('click', function(){
     $('#blend').show()
 })
 
+//Refactor: Turn this into a function:
+var years = document.createElement('select')
+years.id = 'years'
+years.name = 'years'
+years.multiple = 'multiple'
 
+var thisYear = new Date().getFullYear()
+
+for (var y = 2011; y <= thisYear; y++){
+    var option = document.createElement('option')
+    option.value = y
+    option.text  = y
+    years.appendChild(option)
+}
+
+$('#select').prepend(years)
+
+$('#years').multiselect({
+    includeSelectAllOption: true,
+    buttonWidth: '160px',
+    buttonText: function(options, select){
+        var len = select[0].options.length
+
+        if (options.length === 0){
+            return 'Select Years'
+        } else if (options.length === len){
+            return 'All Years'
+        } else if (options.length > 2){
+            return options.length + ' years selected'
+        } else {
+            var labels = [];
+            options.each(function(){
+                if ($(this).attr('label') !== undefined) {
+                    labels.push($(this).attr('label'))
+                } else {
+                    labels.push($(this).html())
+                }
+            })
+
+            return labels.join(', ') + ''
+        }
+    }
+})
+
+$('#years').multiselect('selectAll', false)
+$('#years').multiselect('updateButtonText')
+
+// move years over to make it and facilties into a combo button visually
+$('#years').next('.btn-group').css({ 'marginLeft': '-1px' })
 
 
 ///////////////////
@@ -162,55 +210,6 @@ async.waterfall([
         select.id = 'facilities'
         select.name = 'facilities'
         select.multiple = 'multiple'
-
-        var years = document.createElement('select')
-        years.id = 'years'
-        years.name = 'years'
-        years.multiple = 'multiple'
-
-        var thisYear = new Date().getFullYear()
-
-        for (var y = 2011; y <= thisYear; y++){
-            var option = document.createElement('option')
-            option.value = y
-            option.text  = y
-            years.appendChild(option)
-        }
-
-        $('#select').prepend(years)
-
-        $('#years').multiselect({
-            includeSelectAllOption: true,
-            buttonWidth: '160px',
-            buttonText: function(options, select){
-                var len = select[0].options.length
-
-                if (options.length === 0){
-                    return 'Select Years'
-                } else if (options.length === len){
-                    return 'All Years'
-                } else if (options.length > 2){
-                    return options.length + ' years selected'
-                } else {
-                    var labels = [];
-                    options.each(function(){
-                        if ($(this).attr('label') !== undefined) {
-                            labels.push($(this).attr('label'))
-                        } else {
-                            labels.push($(this).html())
-                        }
-                    })
-
-                    return labels.join(', ') + ''
-                }
-            }
-        })
-
-        $('#years').multiselect('selectAll', false)
-        $('#years').multiselect('updateButtonText')
-
-        // move years over to make it and facilties into a combo button visually
-        $('#years').next('.btn-group').css({ 'marginLeft': '-1px' })
 
         // filter out certain workbooks
         files = files.filter(function(f){
